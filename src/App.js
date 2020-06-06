046725d409d80f14391fb3791fe46714feba918d
hayatniazi <66178207+hayatniazi@users.noreply.github.com>
2020-06-06 20:58:20 +0100

    Added lists

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import './App.css';
 function App() {
   return (
     <div>AssalamoAlikum Hayat Muhammad Niazi!</div>
+    <h1>Example of ordered and unordered list.</h1>
+    <h3>Ordered list</h3>
+    <ol>
+      <li>Pakistan</li>
+      <li>Saudi Arabia</li>
+      <li>UAE</li>
+      <li>Australia</li>
+    </ol>
+
+<h3>Unrdered list</h3>
+<ul>
+  <li>Pakistan</li>
+  <li>Saudi Arabia</li>
+  <li>UAE</li>
+  <li>Australia</li>
+</ul>
   );
 }
 
